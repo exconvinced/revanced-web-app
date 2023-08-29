9bd85d3b8680433b3e26cf1c066d3df4478c042e
Exconvinced <vulaurente@gmail.com>
2023-08-29 23:02:53 +0800

    Report patch termination on error

--- static/script.js
+++ static/script.js
@@ -1,42 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
     const patchButton = document.getElementById('patch-button');
+    const initialPatchButtonText = patchButton.innerHTML;
     const downloadButton = document.getElementById('download-button');
     const terminal = document.getElementById('terminal');
     const terminalWindow = document.getElementById('terminal-window');
     
 
+    function resetPatchButton() {
+        patchButton.innerHTML = initialPatchButtonText
+    }
+    function enablePatchButton() {
+        patchButton.classList.remove('disabled-button');
+        patchButton.disabled = false;
+    }
+    function disablePatchButton() {
+        patchButton.classList.add('disabled-button');
+        patchButton.disabled = true;
+    }
+    function hideDownloadButton() {
+        downloadButton.classList.add('hidden');
+    }
+    function showDownloadButton() {
+        downloadButton.classList.remove('hidden');
+    }
+
+
     // On click of patch button
     patchButton.addEventListener('click', function() {
-        // Disable patch button during patching
-        patchButton.disabled = true;
-        patchButton.classList.add('disabled-button');
-        const patchText = this.innerHTML;
+        disablePatchButton();
         patchButton.textContent = 'Patching...';
+        hideDownloadButton();
 
-        // Hide download button if visible
-        downloadButton.classList.add('hidden');
-
-        // Execute Revanced patch via progress route
         const eventSource = new EventSource('/progress');
         terminal.innerHTML = '';
         
-        // On message from server, print console output
         eventSource.onmessage = function (event) {
             const data = JSON.parse(event.data);
+
             if (data.error) {
-                downloadButton.classList.add('hidden');
+                hideDownloadButton();
+                resetPatchButton();
+                enablePatchButton();
                 eventSource.close();
+                data.data = data.error
             }
+
             terminal.innerHTML += data.data + '<br>';
             terminalWindow.scrollTop = terminalWindow.scrollHeight;
         };
 
         // On error, close connection reveal download button and restore patch button
-        eventSource.onerror = function (event) {
+        eventSource.onerror = () => {
             eventSource.close();
-            downloadButton.classList.remove('hidden');
+            showDownloadButton();
+            resetPatchButton();
             downloadButton.click();
-            patchButton.innerHTML = patchText;
         };
     });
 });
