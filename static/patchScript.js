16f5ce30e9122c424b342848027e9c1b594465ef
Exconvinced <vulaurente@gmail.com>
2023-08-30 10:48:32 +0800

    Refactor script.js -> patchScript.js

---
+++ static/patchScript.js
@@ -0,0 +1,39 @@
+import { patchButton, resetPatchButton, enablePatchButton, disablePatchButton } from "./patchButton.js";
+import { hideDownloadButton, showDownloadButton, clickDownloadButton } from "./downloadButton.js";
+import { terminal, terminalWindow } from "./terminal.js";
+import { beginPatch } from "./patchList.js";
+
+document.addEventListener('DOMContentLoaded', function() {
+    patchButton.addEventListener('click', function() {
+        beginPatch();
+        disablePatchButton('Patching...');
+        hideDownloadButton();
+
+        const eventSource = new EventSource('/progress');
+        terminal.innerHTML = '';
+        
+        eventSource.onmessage = function (event) {
+            const data = JSON.parse(event.data);
+
+            if (data.error) {
+                hideDownloadButton();
+                resetPatchButton();
+                enablePatchButton('Patch failed!');
+                eventSource.close();
+                data.data = data.error
+            }
+
+            terminal.innerHTML += data.data + '<br>';
+            terminalWindow.scrollTop = terminalWindow.scrollHeight;
+        };
+
+        // On error, close connection reveal download button and restore patch button
+        eventSource.onerror = () => {
+            eventSource.close();
+            resetPatchButton();
+            showDownloadButton();
+            clickDownloadButton();
+            console.log("Downloading patched APK...")
+        };
+    });
+});
