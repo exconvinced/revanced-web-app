6a4170b3f916f3dae9ca6cb1a6c1a30e6b2e1220
Exconvinced <vulaurente@gmail.com>
2023-08-30 22:23:55 +0800

    Store ReVanced directories as object

--- helper/revanced.py
+++ helper/revanced.py
@@ -1,29 +1,22 @@
 # revanced.py
 # This script gives the directories of ReVanced dependencies
 
-from os.path import join, exists, makedirs
-from os import getenv
+from os.path import join
+from os import makedirs
 
-makedirs(join("bin", "revanced"))
-makedirs(join("bin", 'apk'))
+makedirs(join("bin", "revanced"), exist_ok=True)
+makedirs(join("bin", 'apk'), exist_ok=True)
 
 REVANCED_DIR = join("bin", "revanced")
 APK_DIR = join("bin", 'apk')
 
-CLI_VER = "2.22.0"
-PATCHES_VER = "2.187.0"
-INTEGRATIONS_VER = "0.117.0"
-
-# give github api url equivalent of this
-# https://github.com/ReVanced/revanced-cli/releases/download/v3.0.1/revanced-cli-3.0.1-all.jar
-# https://api.github.com/repos/ReVanced/revanced-cli/releases/tags/v2.22.0
 
 class Revanced:
     def __init__(self):
-        self.cli          = join(REVANCED_DIR, f'revanced-cli-{CLI_VER}.jar')
-        self.patches      = join(REVANCED_DIR, f'revanced-patches-{PATCHES_VER}.jar')
-        self.patches_json = join(REVANCED_DIR, f'revanced-patches-{PATCHES_VER}.json')
-        self.integrations = join(REVANCED_DIR, f'revanced-integrations-{INTEGRATIONS_VER}.apk')
+        self.cli          = join(REVANCED_DIR, f'revanced-cli.jar')
+        self.patches      = join(REVANCED_DIR, f'revanced-patches.jar')
+        self.patches_json = join(REVANCED_DIR, f'revanced-patches.json')
+        self.integrations = join(REVANCED_DIR, f'revanced-integrations.apk')
         self.options      = join(REVANCED_DIR, 'revanced-options.json')
 
         self.unpatched = join(APK_DIR, 'unpatched.apk')
