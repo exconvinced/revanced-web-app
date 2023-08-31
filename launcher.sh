3e9609918f482c70c0c5c7d0d8c25401d02a91f7
Exconvinced <vulaurente@gmail.com>
2023-08-31 10:02:04 +0800

    Add launcher shortcut

---
+++ launcher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source venv/bin/activate
+
+# Check the operating system
+if [ "$(uname)" == "Linux" ]; then
+    # If the OS is Linux, use xdg-open
+    xdg-open "http://127.0.0.1:5000"
+elif [ "$(uname)" == "Darwin" ]; then
+    # If the OS is macOS, use open
+    open "http://127.0.0.1:5000"
+else
+    echo "Unsupported operating system"
+fi
+
+python app.py
