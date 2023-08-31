e025794974e0beded2c4fe47faecd86dad5a9c84
Exconvinced <vulaurente@gmail.com>
2023-08-31 11:30:35 +0800

    Fix pip dependencies

--- .scripts/launchers/launcher.bat
+++ .scripts/launchers/launcher.bat
@@ -1,7 +1,4 @@
 @echo off
 powershell.exe -ExecutionPolicy Bypass -File venv\bin\Activate.ps1
-pip install -r .scripts/requirements.txt
 start "" "http://127.0.0.1:5000"
-
-pip install flask
 python app.py
--- .scripts/launchers/launcher.sh
+++ .scripts/launchers/launcher.sh
@@ -12,5 +12,4 @@ else
     echo "Running on http://127.0.0.1:5000"
 fi
 
-pip install flask
 python app.py
